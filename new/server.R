57745e051bb00131dd4d9cbefdfdfa8ae991b6f9
Rich FitzJohn <rich.fitzjohn@gmail.com>
2018-06-19 14:32:55 +0100

    Save out all data

--- new/server.R
+++ new/server.R
@@ -1,5 +1,8 @@
 source("plot.R")
 
+APP_VERSION <- "1.0.0"
+PATH_OUTPUT <- "contributions"
+
 start_panel <- function() {
   shiny::tagList(
     shiny::includeMarkdown("doc_start.md"),
@@ -54,7 +57,8 @@ end_panel <- function() {
 }
 
 
-validate_date <- function(user_date, real_date) {
+validate_date <- function(user_date, real_date, start_time) {
+  real_date$elapsed <- as.numeric(Sys.time() - start_time, "secs")
   real_date$user <- user_date
   real_date$correct <- check_date(user_date, real_date$date)
   real_date
@@ -89,14 +93,38 @@ render_prev <- function(prev, stats) {
 }
 
 
+update_data <- function(prev, data) {
+  keep <- names(data_cols)
+  if (is.null(data$time_best)) {
+    time_best <- prev$elapsed
+    time_total <- time_best
+  } else {
+    time_best <- min(prev$elapsed, data$best)
+    time_total <- prev$elapsed + data$time_total
+  }
+  prev$date <- format(prev$date, "%d/%m/%Y")
+
+  list(rows = c(data$rows, list(prev[keep])),
+       time_best = time_best,
+       time_total = time_total)
+}
+
 shiny::shinyServer(
   function(input, output, session) {
     values <- shiny::reactiveValues(
+      ## Session identifier
       id = uuid::UUIDgenerate(),
       start_time = Sys.time(),
+      ## Data on the survey
       survey = NULL,
-      data = NULL,
-      date = NULL)
+      ## Used for tracking how long things take
+      timestamp = NULL,
+      ## Current challenge date
+      date = NULL,
+      ## Last challenge date and validation
+      prev = NULL,
+      ## All survey data so far
+      data = list())
 
     ## Here's the logic moving through the sections
     shiny::observeEvent(
@@ -108,8 +136,9 @@ shiny::shinyServer(
 
     shiny::observeEvent(
       input$challenge, {
-        values$survey <- list(input$survey_keyboard_layout,
-                              input$survey_keyboard_input)
+        values$survey <- list(
+          keyboard_layout = input$survey_keyboard_layout,
+          keyboard_input = input$survey_keyboard_input)
         output$typoapp <- shiny::renderUI(challenge_panel())
         values$date <- new_date()
       })
@@ -118,7 +147,9 @@ shiny::shinyServer(
       input$challenge_submit, {
         message("submitting!")
         isolate({
-          values$prev <- validate_date(input$challenge_date, values$date)
+          values$prev <- validate_date(input$challenge_date, values$date,
+                                       values$timestamp)
+          values$data <- update_data(values$prev, values$data)
           values$date <- new_date()
         })
       })
@@ -126,8 +157,10 @@ shiny::shinyServer(
     shiny::observe({
       if (!is.null(values$date)) {
         date <- values$date
+        shiny::updateTextInput(session, "challenge_date", value = "")
         output$date_image <- shiny::renderPlot(
           plot_date(date), width = date$width, height = date$height)
+        values$timestamp <- Sys.time()
       }
     })
 
@@ -138,12 +171,44 @@ shiny::shinyServer(
     })
 
     shiny::observeEvent(
-      input$end,
-      output$typoapp <- shiny::renderUI(end_panel()))
+      input$end, {
+        save_data(values, PATH_OUTPUT)
+        output$typoapp <- shiny::renderUI(end_panel())
+      })
 
     output$typoapp <- shiny::renderUI(
-      start_panel())
+      ##start_panel())
       ## challenge_panel())
-      ##survey_panel())
+      survey_panel())
   }
 )
+
+data_cols <- list(date = character(1),
+                  user = character(1),
+                  date_format = character(1),
+                  elapsed = numeric(1),
+                  correct = logical(1))
+
+
+data_to_table <- function(data) {
+  rows <- Map(
+    function(name, type) vapply(data$rows, "[[", type, name),
+    names(data_cols), unname(data_cols))
+  as.data.frame(rows, stringsAsFactors = FALSE)
+}
+
+
+save_data <- function(values, path) {
+  if (!is.null(values$data)) {
+    message(sprintf("Saving data for '%s'", values$id))
+    dir.create(path, FALSE, TRUE)
+    ret <- list(id = values$id,
+                start_time = values$start_time,
+                app_version = APP_VERSION,
+                survey = values$survey,
+                data = data_to_table(values$data))
+    dest <- file.path(path, sprintf("%s.rds", ret$id))
+    saveRDS(ret, dest)
+    values$data <- NULL
+  }
+}
