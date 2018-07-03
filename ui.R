4708b641e96baf1d5e9b50b98289d3fe57f3e52f
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-07-03 08:36:09 +0100

    Add footer to each page

--- ui.R
+++ ui.R
@@ -1,5 +1,7 @@
 shiny::shinyUI(
   shiny::fluidPage(
     shinyjs::useShinyjs(),
+    shiny::tags$style(shiny::HTML(".footer {color: #666666; line-height: 80%; font-size: 80%;}")),
     shiny::titlePanel("The Typo Challenge"),
-    shiny::uiOutput("typoapp")))
+    shiny::uiOutput("typoapp"),
+    shiny::includeHTML("footer.html")))
