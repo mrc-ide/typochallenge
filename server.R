b5f6dbc828df57eb6496e2d134a164695e0b4368
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-07-04 15:42:55 +0100

    Remove placeholder modal
    :joy:

--- server.R
+++ server.R
@@ -17,22 +17,6 @@ start_panel <- function() {
     shiny::includeHTML("include/doc_sharing.html"))
 }
 
-dataModal <- function(failed = FALSE) {
-  modalDialog(
-    textInput("dataset", "Choose data set",
-              placeholder = 'Try "mtcars" or "abc"'
-    ),
-    span('(Try the name of a valid data object like "mtcars", ',
-         'then a name of a non-existent object like "abc")'),
-    if (failed)
-      div(tags$b("Invalid name of data object", style = "color: red;")),
-    
-    footer = tagList(
-      modalButton("Cancel"),
-      actionButton("ok", "OK")
-    )
-  )
-}
 
 consent_panel <- function() {
   consent_msg <- shiny::strong("By ticking this box, you are agreeing for us to store and analyse your data, and to make this data publically available.")
